6c4d6d1d90ceb20537f346c4fedc6f7e88e7657f
dadoris <dadoris@uw.edu>
2020-05-14 03:49:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function multiply(number1, number2){
 function divide(number1, number2){
   return number1 / number2
 }
+
+function increment(number1){
+  return number1++
+}
+
+
+function decrement(number1){
+  return number1--
+}
